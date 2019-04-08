f18b1dd477a1e8f3c2b44d8c1daaaed14766342c
tomo0314 <you@example.com>
2019-04-08 18:28:54 +0900

    書き込みのデーベース作成/データベースの内容をページに反映/CSRF対策途中）

--- index.php
+++ index.php
@@ -1,23 +1,65 @@
 <?php
 
-$dataFile="board.dat";
+$dataFile="bbs.dat";
 
-if($_SERVER["REQUEST_METHOD"]==="POST"){
+/*
+//CSRF対策
 
-    $message=$_POST["message"];
-    $user= $_POST["user"];
+session_start();
 
-    $newData= $massage . "\t" . $user . "\n";
-
-    $fp=fopen($dataFile "a");
-    fwrite($fp, $newData);
-    fclose($fp);
+function setToken(){
+    $token=sha1(uniqid(mt_rand(),true));
+    $_SESSION["token"]=$token;
 }
+
+function checkToken(){
+    if(empty($_SESSION["token"]) || ($_SESSION["token"]!= $_POST["token"])){
+        echo "不正なPOSTが行われました";
+        exit;
+    }
+}
+
+*/
+
+function h($s){
+    return htmlspecialchars($s,ENT_QUOTES,"utf-8");
+}
+
+if ($_SERVER["REQUEST_METHOD"]==="POST" &&
+isset($_POST["user"]) && 
+isset($_POST["message"])){
+
+    // checkToken();
+
+    $user= trim($_POST["user"]);
+    $message=trim($_POST["message"]);
+
+    if ($message !== ''){
+       
+        if($user ===""){
+            $user="名無しさん";
+        }else{
+            $user=$user;
+        }
+        $user= str_replace("\t", ' ',$user);
+        $message= str_replace("\t", ' ',$message);
+        $postedAt=date("Y-m-d:i:s");
+        
+        $newData= $user."\t" . $message."\t" . $postedAt."\n";
+        
+        $fp=fopen($dataFile, "a");
+        fwrite($fp, $newData);
+        fclose($fp);
+    }
+    }
+// else{
+// setToken();
+//  }
+
+$posts=file($dataFile, FILE_IGNORE_NEW_LINES);
+$posts=array_reverse($posts);
 ?>
-
-
-
-
+ 
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -35,14 +77,27 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
         <form action="" method="post">
             名前：<input type="text" name="user" value=""><br>
             本文：<textarea name="message" cols="30" rows="3" maxlength="80" wrap="hard" placeholder="80文字以内で入力せよ！" ></textarea>
+            <input type="submit" value="投稿">
+            <!-- <input type="hidden" name="token" vlaue="<?php echo h($_SESSION["token"]); ?>"> -->
         </form>
     </section>
 
     <section>
-        <h2>これまでの投稿一覧</h2>
-        <p>投稿はまだありません</p>
+        <h2>これまでの投稿一覧(<?php echo count($posts); ?>件)</h2>
+        <ul>
+            <?php if (count($posts)): ?>
+                <?php foreach ($posts as $post): ?>
+                <?php list($user,$message,$postedAt)=explode("\t", $post); ?>
+                    <li>(<?php echo h($user); ?>):<?php echo h($message); ?>-<?php echo h($postedAt); ?></li>
+                <?php endforeach ?>
+                   
+            <?php else : ?> 
+            <li>まだ投稿はありません</li>
+            <?php endif; ?>
+            
+        </ul>
+
     </section>
 </body>
-
 
 </html>
